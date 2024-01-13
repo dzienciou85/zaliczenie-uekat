dcb03cc0de278303efe66844b201e5372c3312c2
Krzysztof Bystrzykowski <krzysztof.bystrzykowski@edu.uekat.pl>
2024-01-13 15:58:12 +0100

    correction

--- main.py
+++ main.py
@@ -9,27 +9,23 @@ print(img.shape)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
+# initialize the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
-#image = imutils.resize(img, width=min(400, img.shape[1]))
-orig = img.copy()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+# load image
+image = cv2.imread('pracownicy.jpg')
+image = cv2.resize(image, (700, 400))
+
 # detect people in the image
-(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),padding=(8, 8), scale=1.05)
-	# draw the original bounding boxes
+(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+
+# draw the bounding boxes
 for (x, y, w, h) in rects:
-	cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
-	# apply non-maxima suppression to the bounding boxes using a
-	# fairly large overlap threshold to try to maintain overlapping
-	# boxes that are still people
-	rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-	pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
-	# draw the final bounding boxes
-	for (xA, yA, xB, yB) in pick:
-		cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
-	# show some information on the number of bounding boxes
-	filename = imagePath[imagePath.rfind("/") + 1:]
-	print("[INFO] {}: {} original boxes, {} after suppression".format(
-		filename, len(rects), len(pick)))
-	# show the output images
-cv2.imshow("Before NMS", orig)
-cv2.imshow("After NMS", image)
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+print(f'Found {len(rects)} humans')
+
+# show the output images
+cv2.imshow("People detector", image)
 cv2.waitKey(0)
