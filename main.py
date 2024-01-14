afc888d9baaf88d68526525ad1efc7b24a1ab09e
Krzysztof Bystrzykowski <krzysztof.bystrzykowski.edu.uekat.pl>
2024-01-14 12:23:18 +0100

    removing some uncecessary lines, adding some comments

--- main.py
+++ main.py
@@ -1,36 +1,20 @@
+#import OpenCV
 import cv2
+#import Flask and Flas restful modules
 from flask import Flask
 from flask_restful import Resource, Api
+#Read the image and checking its type
 img = cv2.imread('pobrane.jpg')
 img = cv2.resize(img, (600, 450))
 
 print(type(img))
 print(img.shape)
 
-# cv2.imshow('image', img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+
 
 # initialize the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-
-# # load image
-# image = cv2.imread('pobrane.jpg')
-# image = cv2.resize(image, (600, 450))
-#
-# # detect people in the image
-# (rects, weights) = hog.detectMultiScale(image, winStride=(1, 1), padding=(8, 8), scale=1.04)
-#
-# # draw the bounding boxes
-# for (x, y, w, h) in rects:
-#     cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-# print(f'Found {len(rects)} humans')
-
-# show the output images
-# cv2.imshow("People detector", image)
-# cv2.waitKey(0)
 
 app = Flask(__name__)
 api = Api(app)
